2d3b823f29f4c21055c7a6cc4c142a9cc12993e2
favoured mwange <mwangefavoured@gmail.com>
2024-06-28 23:02:33 +0300

    creates expression and arrow functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function (){
+   let a=2000
+   let b= 100
+    return a/b 
+}
+
+const square=(x) =>{
+     x=2
+    return x**2
+}
+
+const add=(a,b) => {
+     a=3
+     b=4
+     return a+b
+}
